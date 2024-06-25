154cd72d68507a28873405137602f2ccce14a814
crux55 <code@andruquinn.com>
2024-06-25 11:11:23 +1000

    changing event type passed to approve event

--- bot.py
+++ bot.py
@@ -161,7 +161,7 @@ def approve_event(event: Event):
 
         # Execute the query to update the status
         cursor.execute(update_query, (
-            str(event.status), event.event_id, event.event_forum_url, event.event_forum_id,
+            str(STATUS.APPROVED), event.event_id, event.event_forum_url, event.event_forum_id,
             id
         ))
 
