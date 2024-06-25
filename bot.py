78d4a251ac3dd75aa3ce14298900434f0c4d4f51
crux55 <code@andruquinn.com>
2024-06-25 16:01:54 +1000

    fixing update hypeman

--- bot.py
+++ bot.py
@@ -375,7 +375,7 @@ class Hypeman_Approval_Message(discord.ui.View):
             # Handle other exceptions
             log_error(f"An unexpected error occurred: {e}")
     
-    @discord.ui.button(label="Disagree", style=discord.ButtonStyle.red, custom_id="reject")
+    @discord.ui.button(label="Disagree", style=discord.ButtonStyle.red, custom_id="disagree")
     async def on_reject(self, interaction: discord.Interaction, button: discord.ui.Button):
         try:
             self.stop()
